d4a9e8881f1fb7782423048787729b02cc2431d5
aswani <aswani.gunji88@gmail.com>
2024-08-25 18:50:57 +0100

    bucket version

--- day-4/main.tf
+++ day-4/main.tf
@@ -6,4 +6,10 @@ resource "aws_s3_bucket" "name" {
     bucket = "aswani-123"
   
 }
+resource "aws_s3_bucket_versioning" "name" {
+    bucket = aws_s3_bucket.name.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
   
